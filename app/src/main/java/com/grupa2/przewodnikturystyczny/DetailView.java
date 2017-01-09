9f6205e5709b65032237cabbb319d515f5ca4bd3
Krzysiek <synulewar@gmail.com>
2017-01-09 21:07:24 +0100

    Element listy i szkielet DetailView

---
+++ app/src/main/java/com/grupa2/przewodnikturystyczny/DetailView.java
@@ -0,0 +1,13 @@
+package com.grupa2.przewodnikturystyczny;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+}
